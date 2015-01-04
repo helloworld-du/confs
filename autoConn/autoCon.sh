97a2a1b1551dd1dbd704c90a8bb88877da526e3b
dushengchen <dushengch100@126.com>
2015-01-04 14:51:58 +0800

    add echo for local

--- autoConn/autoCon.sh
+++ autoConn/autoCon.sh
@@ -3,6 +3,7 @@ remote=$1
 source ./autoCon.inc.sh
 source ${incBase}/${remote}
 datetime=`date`
+echo "${datetime} ${HOSTNAME} $remote"
 ssh $1 "echo ${HOSTNAME} ${datetime} >> /tmp/connect.log"
 ssh -N -f -R $remotePort:localhost:22 $1
 ssh -qTfnN -D $localVPNPort $1
