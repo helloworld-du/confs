33be2c33f4fd3629bc63702c3e53ea5533c9b8f6
dushengchen <dushengch100@126.com>
2015-04-23 09:53:11 +0800

    add

--- autoConn/proxy.sh
+++ autoConn/proxy.sh
@@ -3,9 +3,9 @@ baseDir=`dirname $0`
 source $baseDir/../base
 
 set -x
+ssh -qTfnN -D 7072 vpn
 ssh -qTfnN -D 7070 molly
 ssh -qTfnN -D 7071 qa
-ssh -qTfnN -D 7072 vpn
 ssh -qTfnN -D 7073 sonia
 ssh -qTfnN -D 7074 julia
 ssh -qTfnN -D 7075 sigma
